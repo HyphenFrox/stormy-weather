a3614e4132c10bfef3ca75702c63466db9ebe4b4
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-14 20:46:01 +0530

    display current temperature

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,22 +1,66 @@
 import { makeStyles, Paper, Typography } from "@material-ui/core";
 import React from "react";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   rootPaper: {
     minHeight: 200,
     display: "flex",
     justifyContent: "center",
+    alignItems: "stretch",
+    padding: "2em",
+    [theme.breakpoints.up("sm")]: {
+      maxWidth: "80%",
+      margin: "auto",
+    },
+  },
+  currentTempSection: {
+    display: "flex",
+    alignContent: "center",
+    alignItems: "stretch",
+    "& > *:first-child": {
+      flexShrink: 0,
+    },
+    "& > *:nth-child(2)": {
+      flexGrow: 1,
+    },
+  },
+  conditionIcon: {
+    width: 100,
+    height: 100,
+    margin: "auto",
+  },
+  tempNumberSection: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
     alignItems: "center",
   },
-});
+}));
 
-function WeatherData() {
+function WeatherData(props) {
+  const { weatherData } = props;
   const classes = useStyles();
   return (
     <Paper className={classes.rootPaper}>
-      <Typography variant="h4" align="center" style={{ fontSize: "1.6rem" }}>
-        Work In Progress
-      </Typography>
+      <div className={classes.currentTempSection}>
+        <img
+          src={weatherData.current.condition.icon}
+          className={classes.conditionIcon}
+        ></img>
+        <div className={classes.tempNumberSection}>
+          <Typography
+            variant="h4"
+            align="center"
+            style={{ fontSize: "2.5rem" }}
+          >
+            {weatherData.current.temp_c}
+          </Typography>
+          <Typography variant="h4" style={{ fontSize: "2.5rem" }}>
+            <sup>o</sup>C
+          </Typography>
+        </div>
+      </div>
     </Paper>
   );
 }
